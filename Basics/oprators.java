38a775b9e4467640bc7183b9f1cdcb86e9e56abd
saurabhexe <saurabhtayde1234@gmail.com>
2022-08-10 16:18:12 +0530

    modified:   Basics/oprators.java

--- Basics/oprators.java
+++ Basics/oprators.java
@@ -1,17 +1,20 @@
 package Basics;
-
-public class oprators{
+public class oprators {
     public static void main(String[] args) {
         
-        int a = 10, b = 15;
-        
-        int add = a + b; System.out.println(add);
-        int sub = a - b; System.out.println(sub);
-        int multi = a * b; System.out.println(multi);
-        int divison = a / b; System.out.println(divison);
-        int remender = a % b; System.out.println(remender);
+        int a = 10, b = 20;
 
-        
+        int sum = a + b;
+        int sub = a - b;
+        int multiplication = a * b;
+        int division = a / b;
+        int remainder = a % b;
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(multiplication);
+        System.out.println(division);
+        System.out.println(remainder);
 
     }
 }
