4abcd82fc96d0589539bcdc311d80a2205bbcdaf
saurabhexe <saurabhtayde29@gmail.com>
2022-08-13 21:42:14 +0530

    initially added

---
+++ Basics/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name = "minamaanni";
+        System.out.println(name);
+
+        String surname = "Patil";
+        System.out.println(surname);
+
+        System.out.println("my name is "+name + surname);
+    }
+}
